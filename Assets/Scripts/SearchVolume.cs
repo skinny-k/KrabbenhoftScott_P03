e927107bc6c87c7a7792291a7bffdf89e5794e62
Skinny <scott.krabbenhoft02@gmail.com>
2021-05-05 13:41:11 -0500

    Detection Improvements (#1)
    Fixed a glitch where enemies would be able to see a player after they left detection range.

--- Assets/Scripts/SearchVolume.cs
+++ Assets/Scripts/SearchVolume.cs
@@ -41,6 +41,10 @@ public class SearchVolume : MonoBehaviour
                     canSeePlayer = false;
                 }
             }
+            else
+            {
+                canSeePlayer = false;
+            }
         }
     }
     
